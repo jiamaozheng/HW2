a97c15bddf0d8fd783ac9049a0ca2b1465625d46
jiamaozheng <jiamaozheng@users.noreply.github.com>
2014-07-17 19:14:00 +0000

    Delete DieTests.java

--- DieTests.java
+++
@@ -1,9 +0,0 @@
-public class DieTests {
-    public static void main (String[] args){
-	Die die1 = new DieImpl(4);
-
-	System.out.println("The number of sides is " + die1.nSides());
-	System.out.println("The number of roll is " + die1.roll());
- }
-
-}
