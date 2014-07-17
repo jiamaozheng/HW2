352ea51ab423ed2b617b63a048734767ec31ef7b
jiamaozheng <jiamaozheng@users.noreply.github.com>
2014-07-17 19:13:06 +0000

    Delete CMYK.java

--- CMYK.java
+++
@@ -1,9 +0,0 @@
-public interface CMYK {
-    public double getC();
-    public double getM();
-    public double getY();
-    public double getK();
-    public CMYK negate ();
-    public RGB toRGB ();
-    public String toString ();
-}
